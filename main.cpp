2fddab8a414cc25006166c6693b192e0d25d6095
David <dsoriano7787@sdsu.edu>
2025-02-13 20:19:42 -0800

    completed

--- main.cpp
+++ main.cpp
@@ -109,18 +109,19 @@ public:
     void deleteNode(int i) {
        //TODO:Write the function to delete at the given index. Reuse the pre-written functions for edge cases. Account for missing index.
         if (i<0 || i>=length) {
-            return;
+            return; // edge case
         }
         if (i==0) {
-            return delfirst();
+            return delfirst(); // since the index you chose to delete is 0, you just delete the first node which you can use delete first for
         }
         if (i==length-1) {
-            return dellast();
+            return dellast(); // if the index you chose is exactly the last node, you can just use delete last
         }
-        Node<T> *prev = get(index - 1);
-        Node<T> *temp = prev->next;
-        prev->next = temp->next;
-        delete temp;
+        Node<T> *prev = get(i-1); // gets previous through subtracting one from chosen index and using get method to take it
+        Node<T> *temp = prev->next; // sets temp to where your index is so you can move your prev
+        prev->next = temp->next; // move next of prev
+        delete temp; // deleteing temp
+        length--; // takes out lenght that was deleted
     }
 
    void reverselist(){
